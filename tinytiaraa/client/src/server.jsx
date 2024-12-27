864af8c824a11c333e328c7152c1eaddc9a7fc8b
Web Development2 <Web Development2@ru.local>
2024-12-27 15:05:46 +0530

    testing api codess

--- tinytiaraa/client/src/server.jsx
+++ tinytiaraa/client/src/server.jsx
@@ -1,7 +1,7 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://tiny-new.vercel.app/api/v2";
 
 
-export const backend_url = "http://localhost:8000/"
+export const backend_url = "https://tiny-new.vercel.app/"
 
 
 export const imgdburl = "https://backend.tinytiaraa.com:8000"
