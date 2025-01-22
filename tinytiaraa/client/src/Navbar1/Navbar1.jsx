c7cc713b38726ec9597c885dcfec7553f9ae1357
Web Development2 <Web Development2@ru.local>
2025-01-22 16:43:14 +0530

    new backend deploy chatbots razorpay testingsssssssssss

--- tinytiaraa/client/src/Navbar1/Navbar1.jsx
+++ tinytiaraa/client/src/Navbar1/Navbar1.jsx
@@ -1174,20 +1174,35 @@ function Navbar1() {
                         </Slider>
 
                     </div>
-                    <div className='currencysection !mr-10 w-[10%]' onClick={toggleCurrencySelector} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
-                        <div className='headcurrencymain ml-6'>
+                    <div 
+                className='!mr-10' 
+                onClick={toggleCurrencySelector} 
+                style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+                {currencyDataz.length > 0 && (
+                    <img 
+                        src={currencyDataz.find(currency => currency.code === selectedCurrency)?.flag} 
+                        alt={`${selectedCurrency} flag`} 
+                        className="w-5 h-5 mr-2" 
+                    />
+                    
+                )}
+                <span className='font-[400]'>{selectedCurrency}</span>
+            </div>
 
-                            {/* <img src={currencyData[selectedCurrency].flag} alt={`${selectedCurrency} flag`} className="w-5 h-5 mr-2" /> */}
-                                    {currencyDataz.length > 0 && (
-                            <img 
-                                src={currencyDataz.find(currency => currency.code === selectedCurrency)?.flag} 
-                                alt={`${selectedCurrency} flag`} 
-                                className="w-5 h-5 mr-2" 
-                            />
-                        )}
-                            <span className='headcurrencycode font-[400]'>{selectedCurrency}</span>
-                        </div>
-                        </div>
+            {
+                currencyDataz.length > 0 &&  (
+                    <div style={{display:"none"}}>
+
+                    <CurrencySelector  onCurrencySelect={handleCurrencySelect} toggleCurrencySelector={toggleCurrencySelector}  currencyDataz={currencyDataz || []}  />
+                    </div>
+
+                )
+            }
+
+
+
+         
+
                         {isCurrencySelectorOpen && (
                             <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                             <div className="bg-white px-3 pb-6 pt-2 rounded-lg shadow-lg max-w-sm w-full text-black" style={{ boxShadow: "0px 10px 15px rgba(0, 0, 0, 0.3)" }}>
@@ -1197,7 +1212,7 @@ function Navbar1() {
 
                                 </div>
                              
-                            <CurrencySelector onCurrencySelect={handleCurrencySelect} toggleCurrencySelector={toggleCurrencySelector} />
+                            <CurrencySelector onCurrencySelect={handleCurrencySelect} toggleCurrencySelector={toggleCurrencySelector}  currencyDataz={currencyDataz || []}  />
                             </div>
                           </div>
 
